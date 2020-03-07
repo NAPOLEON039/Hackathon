dbb62817d0f27b4fae17aa141cd1818e52f5019f
NAPOLEON039 <nihar.raote@gmail.com>
2020-03-08 00:20:21 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <div v-if="login" id="login">
+    <h1>Police Safety Control</h1>
+    <input id="pin" type="password" placeholder="Enter pin">
+    <button @click="signin" id="btn">Login</button>
   </div>
+  <div id="main" v-else>
+    <ol :key = "index" v-for="(location, index) in locations">
+      <li>{{location}}</li>
+    </ol>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import database from './firebaseInit'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      login: true,
+      locations: []
+    }
+  },
+  methods: {
+    signin() {
+      this.login = !this.login
+      database.collection('entries').get().then(data => {
+        data.docs.map(doc => {
+          this.locations.push(doc.data().location)
+        })
+        data.docs.map(res => {
+          database.collection('entries').doc(res.id).onSnapshot(doc => {
+            alert('Help is needed!')
+          })
+        })
+      })
+      database.collection('entries')
+    }
   }
 }
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+}
+
+#login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+h1 {
+    margin-top: 20px;
+    font-size: 5em;
+}
+
+#pin {
+    width: 250px;
+    height: 30px;
+    border-radius: 5px;
+    margin-top: 50px;
+}
+
+#pin:focus {
+    outline-style: none;
+    box-shadow: 0 0 1pt 1pt rgb(26, 96, 224);
+}
+
+#btn {
+    width: 100px;
+    font-size: 1em;
+    margin-top: 15px;
+    padding: 5px 20px;
+    border-radius: 5px;
+    background-color: rgb(79, 131, 226);
+    color: white;
+}
+
+#btn:focus {
+    outline-style: none;
+    box-shadow: 0 0 1pt 1pt rgb(79, 131, 226);
 }
 </style>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAZDAiHTpw90azfYYNX3cVQ_btmlGVFHUY",
+    authDomain: "sihfirebaseproject-e2dd6.firebaseapp.com",
+    databaseURL: "https://sihfirebaseproject-e2dd6.firebaseio.com",
+    projectId: "sihfirebaseproject-e2dd6",
+    storageBucket: "sihfirebaseproject-e2dd6.appspot.com",
+    messagingSenderId: "131868880408",
+    appId: "1:131868880408:web:19847459beead56b859d6d"
+};
